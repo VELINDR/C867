d55c6b8b61221b0adfd7bdb69cbe068b7853320a
VELINDR <velagicindira@gmail.com>
2025-01-12 18:05:10 -0600

    degree .h

--- degree .h
+++ degree .h
@@ -3,5 +3,5 @@
 //enum
 enum DegreeProgram {SECURITY, SOFTWARE, NETWORK};
 
-//make sure that the order of the programs match, took a long time to catch that 'software' and 'security' were swapped around 
+//make sure that the order of the programs match
 static const std::string degreeProgramString[] = {"SECURITY", "SOFTWARE", "NETWORK"};
